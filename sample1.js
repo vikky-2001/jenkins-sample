035094a67e992fbcc5ac3ceb154e2777370b455c
vikky-2001 <103255264+vikky-2001@users.noreply.github.com>
2022-04-08 16:43:37 +0530

    update sample1.js

--- sample1.js
+++ sample1.js
@@ -8,7 +8,7 @@ const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
 
 let result;
-
+//j
 // using if...else if... else
 if (operator == '+') {
     result = number1 + number2;
